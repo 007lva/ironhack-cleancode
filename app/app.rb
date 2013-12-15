97b5d5d1dddee0f988c2109f1eb0bab6229e5381
Alex Martín <amartinj@gmail.com>
2013-12-15 16:24:43 +0100

    Extracted methods

--- app/app.rb
+++ app/app.rb
@@ -14,85 +14,87 @@ class App < Sinatra::Base
   end
 
   post '/admin/book' do
-    book = Book.new
-
-    # Validate form data
-    author = @params['author']
-    raise 'Invalid Author' if author.length < 3 || author.length > 30
-    book.author = author
-
-    title = @params['title']
-    raise 'Invalid Title' if title.length < 3 || author.length > 30
-    book.title = title
-
-    year = Integer(@params['year'])
-    raise 'Invalid publish date' if year > DateTime.now.year
-    book.published_at = DateTime.new(year)
-
-    # Set owner
-    owner = User.find(@params['owner'])
-    book.owner = owner
-
-    # Save book
-    book.save
-
+    create_book_from_params(@params)
     redirect to '/admin'
   end
 
   post '/admin/user' do
-    user = User.new
-    user.name = @params['name']
-    user.last_name = @params['last_name']
-
-    # Generate API Key
-    apiKey = "A-#{([nil]*8).map { ((48..57).to_a+(65..90).to_a+(97..122).to_a).sample.chr }.join}"
-    while User.where(api_key: apiKey).exists?
-      apiKey = "A-#{([nil]*8).map { ((48..57).to_a+(65..90).to_a+(97..122).to_a).sample.chr }.join}"
-    end
-    user.api_key = apiKey
-
-    # Save user
-    user.save
-
+    create_user_from_params(@params)
     redirect to '/admin'
   end
 
   get '/api/books' do
     content_type :json
-    apiKey = request.env['HTTP_IRONHACK_API_KEY']
+    api_key = request.env['HTTP_IRONHACK_API_KEY']
 
-    # Get books for user with apiKey
-    user = User.find_by(api_key: apiKey)
-    books = user.books
-    result = []
+    books = find_books_for_user_with_api_key(api_key)
 
-    # Render as json
-    books.each do |book|
-      result << {id: book._id, title: book.title, author: book.author, publish_date: book.published_at.iso8601}
-    end
-
-    result.to_json
+    render_books_as_json(books)
   end
 
   get '/api/books/:id' do
     content_type :json
-    apiKey = request.env['HTTP_IRONHACK_API_KEY']
+    api_key = request.env['HTTP_IRONHACK_API_KEY']
+    book_id = @params[:id]
 
-    # Get book for user with apiKey
-    owner = User.find_by(api_key: apiKey)
-    book = owner.books.find(@params[:id])
+    book =  find_book_with_id_owned_by_user_with_api_key(book_id, api_key)
 
-    # Render as json
-    {id: book._id, title: book.title, author: book.author, publish_date: book.published_at.iso8601}.to_json
+    render_book_as_json(book)
   end
 
   post '/api/books' do
     content_type :json
-    apiKey = request.env['HTTP_IRONHACK_API_KEY']
-    owner = User.find_by(api_key: apiKey)
+    api_key = request.env['HTTP_IRONHACK_API_KEY']
+
+    book = create_book_from_request_for_user_with_api_key(@request, api_key)
+
+    render_book_as_json(book)
+  end
+
+  error Mongoid::Errors::DocumentNotFound do
+    raise not_found
+  end
+
+  private
+  def create_book_from_params(params)
+    book = Book.new
+
+    # Validate form data
+    author = params['author']
+    raise 'Invalid Author' if author.length < 3 || author.length > 30
+    book.author = author
+
+    title = params['title']
+    raise 'Invalid Title' if title.length < 3 || author.length > 30
+    book.title = title
+
+    year = Integer(params['year'])
+    raise 'Invalid publish date' if year > DateTime.now.year
+    book.published_at = DateTime.new(year)
+
+    # Set owner
+    owner = User.find(params['owner'])
+    book.owner = owner
+
+    # Save book
+    book.save
+  end
+
+  def create_user_from_params(params)
+    user = User.new
+    user.name = params['name']
+    user.last_name = params['last_name']
+
+    generate_api_key_for_user(user)
+
+    user.save
+  end
+
+  def create_book_from_request_for_user_with_api_key(request, api_key)
+    owner = User.find_by(api_key: api_key)
 
     # Create book from request
-    requestData = JSON.parse(@request.body.read)
+    requestData = JSON.parse(request.body.read)
     book = Book.new
     book.title = requestData['title']
     book.author = requestData['author']
@@ -102,11 +104,43 @@ class App < Sinatra::Base
     # Save book
     book.save
 
-    # Render as json
-    {id: book._id, title: book.title, author: book.author, publish_date: book.published_at.iso8601}.to_json
+    book
   end
 
-  error Mongoid::Errors::DocumentNotFound do
-    raise not_found
+  def generate_api_key_for_user(user)
+    apiKey = "A-#{([nil]*8).map { ((48..57).to_a+(65..90).to_a+(97..122).to_a).sample.chr }.join}"
+    while User.where(api_key: apiKey).exists?
+      apiKey = "A-#{([nil]*8).map { ((48..57).to_a+(65..90).to_a+(97..122).to_a).sample.chr }.join}"
+    end
+    user.api_key = apiKey
+  end
+
+  ## Find
+  def find_books_for_user_with_api_key(apiKey)
+    # Get books for user with apiKey
+    user = User.find_by(api_key: apiKey)
+    user.books
+  end
+
+  def find_book_with_id_owned_by_user_with_api_key(book_id, api_key)
+    owner = User.find_by(api_key: api_key)
+    owner.books.find(book_id)
+  end
+
+  ## Render
+  def render_books_as_json(books)
+    result = []
+    books.each do |book|
+      result << book_to_hash(book)
+    end
+    result.to_json
+  end
+
+  def render_book_as_json(book)
+    book_to_hash(book).to_json
+  end
+
+  def book_to_hash(book)
+    {id: book._id, title: book.title, author: book.author, publish_date: book.published_at.iso8601}
   end
 end
